de6956eadd38f330a576a53f8f4d5bed6e96ee5f
syeda-sunjida <nikitaferdous1999@gmail.com>
2023-06-10 00:54:58 +0600

    classes edited

--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -4,7 +4,7 @@ const SectionTitle = ({heading}) => {
     return (
         <div className="mx-auto text-center md:w-4/12 my-8">
             
-            <h3 className="text-3xl uppercase border-y-4 py-4 text-orange-400">{heading}</h3>
+            <h3 className="text-3xl uppercase border-y-4 py-4 border-s-orange-100 text-orange-400">{heading}</h3>
         </div>
     );
 };
